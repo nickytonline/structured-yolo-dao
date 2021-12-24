aef4548ce2c37ba0005c39e296a6eb3dabd28252
Nick Taylor <nick@iamdeveloper.com>
2021-12-23 23:51:13 -0500

    Promoted address utlity functoin to the addresses module.

--- utilities/addresses.ts
+++ utilities/addresses.ts
@@ -1,3 +1,9 @@
+export function shortenAddress(address: string) {
+  return (
+    address.substring(0, 6) + '...' + address.substring(address.length - 4)
+  );
+}
+
 export const BUNDLE_DROP_ADDRESS = '0xB6E4932F848A337Ad5FDDfD8a3113fAF90024312';
 export const TOKEN_MODULE_ADDRESS =
   '0xB7e1a5175aacaf7f30A09Bca1dD54e1b94E63Fb4';
